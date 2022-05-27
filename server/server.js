556e74a38ab2b3f5ab2018cf1312786d2dafef45
Kapil Choudhari <kapil.choudhari@ses.com>
2022-05-27 23:32:58 +0530

    added notes router middleware and .env

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const cors = require("cors");
 
 // to be able to access the body in express
 app.use(express.json());
 app.use(cors());
 
+// Routers
 
 app.get("/api/", (req, res) => {
   res.send("hello world");
 });
 
+const notesRouter = require("./routes/Notes");
+app.use("/api/notes", notesRouter);
+
+
+// Start the server
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log('server running on port ' + PORT)
-});
+app.listen(PORT, console.log(`server running on port ${PORT}`));
 
