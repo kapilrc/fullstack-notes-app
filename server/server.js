893d0447509e1976bd65b8498cc35267b7e0e5af
Kapil Choudhari <kapil.choudhari@ses.com>
2022-06-22 14:52:09 +0530

    buildpack changes required in root

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ __dirname = path.resolve();
 console.log(__dirname)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '/client/dist')));
 
   app.get('*', (req, res) => {
     res.send(path.resolve(__dirname, 'client', 'dist', 'index.html'))
